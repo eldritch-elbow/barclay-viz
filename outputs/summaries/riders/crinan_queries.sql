56de416993f9dee2429eb7cbc7c857ee5fc83b7f
James Siddle <j.siddle@digital-science.com>
2014-03-16 08:53:51 +0000

    New queries

--- outputs/summaries/riders/crinan_queries.sql
+++ outputs/summaries/riders/crinan_queries.sql
@@ -26,6 +26,20 @@
 -- DESC limit 600000;
 
 # Crinan commuter
-select * from journeys 
+-- select * from journeys 
+-- where customer_record_number = 1921951825
+-- limit 20000;
+
+# Crinan commuter
+select 
+  start_station_logical_term, 
+  end_station_logical_term, 
+  count(rental_id) as jny_count, 
+  avg(duration) as avg_duration,
+  sum(case end_hour_category_id when 1 then 1 else 0 end) as peak_journeys, 
+  sum(case end_hour_category_id when 0 then 1 else 0 end) as off_peak_journeys
+from journeys 
 where customer_record_number = 1921951825
-limit 20000;
+group by start_station_logical_term, end_station_logical_term
+order by weight DESC
+limit 10000;
