c51419d41c4b517e6d613c84c3a09c8d43e05dcb
James Siddle <j.siddle@digital-science.com>
2014-03-16 11:43:32 +0000

    Aggregate to/from (untested)

--- web_viz/js/viz.js
+++ web_viz/js/viz.js
@@ -1,55 +1,96 @@
 
+/* Create the map */
+var map = L.map('map').setView([51.5211, -0.0788698], 13);
 
-var map = L.map('map').setView([51.505, -0.09], 13);
-
+/* Create a tile layer based on cloudmade */
 L.tileLayer('http://{s}.tile.cloudmade.com/8EE2A50541944FB9BCEDDED5165F09D9/997/256/{z}/{x}/{y}.png', {
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-	maxZoom: 18
-}).addTo(map);
+	attribution: 
+		'Map data &copy; '+
+		'<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '+
+		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '+
+		'Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+	maxZoom: 18})
+.addTo(map);
 
+/* Create objects to hold stations and journey summaries */
 stations = {}
+journeys = {}
 
+/* Store stations, keyed by logical terminal ID */
 $.getJSON( "assets/stations.json", function( station_data ) {
 
 	$.each( station_data, function( key, station ) {
 		stations[station.logical_terminal] = station;
 	});
 
-	$.getJSON( "assets/test_commuter_921951825.json", function( journey_data ) {
+
+	/* Process raw journey data, create summaries */
+	$.getJSON( "assets/crinan_commuter_1921951825.json", function( journey_data ) {
 
 		$.each( journey_data, function( key, journey ) {
 		
-			start = stations[journey.start_station_logical_term]
-			end = stations[journey.end_station_logical_term]
+			start = journey.start_station_logical_term;
+			end = journey.end_station_logical_term;
 
-			start.active = end.active = true;
+			key = Math.min(start,end) + ":" + Math.max(start,end);
 
-			latlngs = [ [ start.latitude, start.longitude ], [ end.latitude, end.longitude ] ]
+			jny_summary = journeys[key];
 
-			var polyline = L.polyline(latlngs, 
-				{color: 'red', weight: journey.jny_count/2}
-			).addTo(map);
+			if (!jny_summary) {
+				jny_summary = {
+					'station_a' : Math.min(start,end),
+					'station_b' : Math.max(start,end),
+					'jny_count' : 0,
+					'avg_duration' : 0,
+					'journey_list' : []
+				}
 
-			polyline.bindPopup(
-				"<b>" + start.full_name + "</b> to <br><b>" + end.full_name + "</b><br>" +
-				journey.jny_count + " journeys<br>" + 
-				Math.ceil(journey.avg_duration / 60) + " minutes (avg)");
-
-		});
-
-		$.each( stations, function( key, station ) {
-
-			if (!station.active) {
-				return true;
+				journeys[key] = jny_summary;
 			}
 
-			var marker = L.circleMarker([station.latitude, station.longitude], {color: 'red', fillColor: '#f03', fillOpacity: 0.5, radius: 3 }).addTo(map);
-			marker.bindPopup(station.full_name);
+			jny_summary['jny_count'] += 1;
+			jny_summary['journey_list'].push(journey);
 
 		});
-		
 	});
 
+	/* Render journeys */
+	$.each( journeys, function( key, journey ) {
+	
+		station_a = stations[journey.station_a];
+		station_b = stations[journey.station_b];
+
+		station_a.active = station_b.active = true;
+
+		latlngs = [ [ station_a.latitude, station_a.longitude ], [ station_b.latitude, station_b.longitude ] ]
+
+		var polyline = L.polyline(
+			latlngs, 
+			{color: 'lightred', weight: journey.jny_count} )
+		.addTo(map);
+
+		polyline.bindPopup(
+			"<b>" + station_a.full_name + "</b> / <br><b>" + station_b.full_name + "</b><br>" +
+			journey.jny_count + " journeys<br>" + 
+			Math.ceil(journey.avg_duration / 60) + " minutes (avg)");
+
+	});
+
+	/* Render stations */
+	$.each( stations, function( key, station ) {
+
+		if (!station.active) {
+			return true;
+		}
+
+		var marker = L.circleMarker(
+			[station.latitude, station.longitude], 
+			{color: 'red', fillColor: '#f03', fillOpacity: 0.5, radius: 3 })
+		.addTo(map);
+		marker.bindPopup("<b>" + station_a.full_name + "</b>");
+
+	});
+		
 });
 
 
